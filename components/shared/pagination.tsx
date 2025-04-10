14b8a0e148c475951128f49b3befff7456818c3f
dimaexzet <exxzet@gmail.com>
2025-04-10 20:28:40 +0200

    Fix type error in pagination component

--- components/shared/pagination.tsx
+++ components/shared/pagination.tsx
@@ -13,10 +13,10 @@ const Pagination = ({ page, totalPages, urlParamName }: PaginationProps) => {
   const router = useRouter();
   const searchParams = useSearchParams();
 
-  const handleClick = (btnType: string) => {
+  const handleNavigation = (btnType: string) => {
     const pageValue = btnType === 'next' ? Number(page) + 1 : Number(page) - 1;
     const newUrl = formUrlQuery({
-      params: searchParams.toString(),
+      params: searchParams?.toString() || '',
       key: urlParamName || 'page',
       value: pageValue.toString(),
     });
@@ -31,7 +31,7 @@ const Pagination = ({ page, totalPages, urlParamName }: PaginationProps) => {
         variant='outline'
         className='w-28'
         disabled={Number(page) <= 1}
-        onClick={() => handleClick('prev')}
+        onClick={() => handleNavigation('prev')}
       >
         Previous
       </Button>
@@ -40,7 +40,7 @@ const Pagination = ({ page, totalPages, urlParamName }: PaginationProps) => {
         variant='outline'
         className='w-28'
         disabled={Number(page) >= totalPages}
-        onClick={() => handleClick('next')}
+        onClick={() => handleNavigation('next')}
       >
         Next
       </Button>
